b12dc0f2a30965e14dbfb1a6f9fa0684a5b40a17
Jamison Hunter <hunterjamison6@gmail.com>
2024-03-21 09:49:57 -0400

    Added Basic Algorithms
    I added FizzBuzz and a number guesser using C#.

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,15 @@
+﻿// This code guesses values between 1 and 10 until it gets the correct answer.
+
+sbyte number = 3;
+
+Random value = new Random();
+
+do{
+    sbyte guess = (sbyte)value.Next(1, 11);
+    Console.WriteLine($"Computer guesses: {guess}");
+    if(guess == number){
+        Console.WriteLine($"The computer guessed {number} correctly!");
+        break;
+
+    }
+} while(true);
